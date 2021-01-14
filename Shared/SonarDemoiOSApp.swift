f68002c4a32d420337e8e2f8e8b9f5fe2d58495f
Ricarlo Silva <ricarlosilv@gmail.com>
2021-01-14 13:01:55 -0300

    first commit

---
+++ Shared/SonarDemoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarDemoiOSApp.swift
+//  Shared
+//
+//  Created by 2104658825 on 14/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SonarDemoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
